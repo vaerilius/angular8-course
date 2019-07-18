9eb11cf978cf390b0d97ab2978a546faa3beab07
Vaerilius <39900747+vaerilius@users.noreply.github.com>
2019-07-18 16:21:59 +0300

    style vs styleUrl

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [` 
+  h3 {
+    color: red;
+  }
+  `]
 })
 export class AppComponent {
   title = 'first-app from appComponent.ts';
