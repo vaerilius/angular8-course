e4242d0f5baeb316094004c22106c3e0ca99abc3
Vaerilius <39900747+vaerilius@users.noreply.github.com>
2019-07-18 15:51:01 +0300

    manual created component -- server component

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
