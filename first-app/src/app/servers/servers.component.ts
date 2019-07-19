72e3850bb444db2f3e5e746ed58f6b128603a6cd
Vaerilius <39900747+vaerilius@users.noreply.github.com>
2019-07-19 20:39:01 +0300

    *ngIf-else

--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Test server';
+  serverCreated = false;
 
   constructor() { 
     setTimeout(() => {
@@ -20,7 +21,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created ' + this.serverName;
   }
   onUpdateServerName(event: any) {
     this.serverName = event.target.value;
