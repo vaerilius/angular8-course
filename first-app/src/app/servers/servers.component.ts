b0d45b3ef8df96f0acb8e7da487a0b3e9f348518
Vaerilius <39900747+vaerilius@users.noreply.github.com>
2019-07-19 19:57:03 +0300

    2 ways databinding

--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test server';
+
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
@@ -14,6 +17,13 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
   }
 
 }
