f69ad25bfc90e3e91083fb7fb2ccd395258d2c18
Vaerilius <tjohannest@gmail.com>
2019-07-26 12:44:43 +0300

    @Input alias

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -6,7 +6,7 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+  @Input('serverElement') element: {type: string, name: string, content: string};
   constructor() { }
 
   ngOnInit() {
