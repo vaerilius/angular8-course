438d672ac5972ae1230abb853166c99dbca3e4b0
Vaerilius <tjohannest@gmail.com>
2019-07-30 19:55:41 +0300

    better way to Attribute Directive + DOM no needed

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
---
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,12 @@
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit{
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+ngOnInit(): void {
+    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+}
+}
