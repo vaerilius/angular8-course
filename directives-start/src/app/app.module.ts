e74e91e4f770204877b3325434d2d0299cee2a56
Vaerilius <tjohannest@gmail.com>
2019-07-30 19:44:18 +0300

    Attribute Directive

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
+import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
