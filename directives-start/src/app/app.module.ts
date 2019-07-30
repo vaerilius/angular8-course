f8ba77261bc76ed26faa90e58e38c4bf91990fb2
Vaerilius <tjohannest@gmail.com>
2019-07-30 20:46:14 +0300

    custom structural directive

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
