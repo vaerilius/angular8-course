b4d1c3b3f6e011c57c36afb67b7502135e44b749
Vaerilius <tjohannest@gmail.com>
2019-08-01 12:03:37 +0300

    rightway to use services, injecting the service

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -4,17 +4,18 @@ import {LoggingService} from "../logging.service";
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
+  constructor(private loggingService: LoggingService) {}
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    const service = new LoggingService();
-    service.logStatusChange(accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
