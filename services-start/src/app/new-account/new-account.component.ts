a18a1677d2a5222ed572c6ff91af6d141e9a6c6f
Vaerilius <tjohannest@gmail.com>
2019-08-01 11:51:16 +0300

    wrong way to use services

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import {LoggingService} from "../logging.service";
 
 @Component({
   selector: 'app-new-account',
@@ -13,6 +14,7 @@ export class NewAccountComponent {
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    const service = new LoggingService();
+    service.logStatusChange(accountStatus);
   }
 }
