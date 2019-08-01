8db8b04d7a9f68b165aca032d44ceea64ffde8a3
Vaerilius <tjohannest@gmail.com>
2019-08-01 13:01:29 +0300

    many Instances of Service

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {LoggingService} from '../logging.service';
 import {AccountsService} from "../accounts.service";
 
@@ -6,16 +6,17 @@ import {AccountsService} from "../accounts.service";
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService, AccountsService]
+  providers: [LoggingService]
 })
 export class AccountComponent {
-  @Input() account: {name: string, status: string};
+  @Input() account: { name: string, status: string };
   @Input() id: number;
 
-  constructor(private loggingService: LoggingService, private  accountsService: AccountsService) {}
+  constructor(private loggingService: LoggingService, private  accountsService: AccountsService) {
+  }
 
   onSetTo(status: string) {
     this.accountsService.updateAccount(this.id, status);
-   this.loggingService.logStatusChange(status);
+    this.loggingService.logStatusChange(status);
   }
 }
--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,12 +1,12 @@
 import {Component} from '@angular/core';
 import {LoggingService} from '../logging.service';
-import {AccountsService} from "../accounts.service";
+import {AccountsService} from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountsService]
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
 
