0309a557e67b9f259aa7629b0683b67c68af9a88
Vaerilius <tjohannest@gmail.com>
2019-08-04 14:24:04 +0300

    Using Services for Pushing Data

--- project/src/app/shopping-list/shopping-list.component.ts
+++ project/src/app/shopping-list/shopping-list.component.ts
@@ -14,6 +14,11 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
   }
 
 }
--- project/src/app/shopping-list/shopping-list.service.ts
+++ project/src/app/shopping-list/shopping-list.service.ts
@@ -1,7 +1,8 @@
 import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from "@angular/core";
 
 export class ShoppingListService {
-
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
  private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Banana', 3)
@@ -9,6 +10,7 @@ export class ShoppingListService {
 
  addIngredient(ingredient: Ingredient) {
    this.ingredients.push(ingredient);
+   this.ingredientsChanged.emit(this.ingredients.slice());
  }
  getIngredients() {
    return this.ingredients.slice();
