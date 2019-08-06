4623d4114d5b12ae0545b91ad15fe74602b7b1d5
Vaerilius <tjohannest@gmail.com>
2019-08-06 08:35:50 +0300

    Feching Route Parameters

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -17,7 +17,7 @@ import {RouterModule, Routes} from "@angular/router";
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UserComponent },
-  { path: 'users/:id', component: UserComponent },
+  { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent }
 ];
 
