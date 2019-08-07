d6b89a797e8d9e576a3acfaf41a8da52e03e4445
Vaerilius <tjohannest@gmail.com>
2019-08-07 08:54:27 +0300

    Nested routes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -1,27 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {UsersComponent} from './users/users.component';
+import {ServersComponent} from './servers/servers.component';
+import {UserComponent} from './users/user/user.component';
+import {EditServerComponent} from './servers/edit-server/edit-server.component';
+import {ServerComponent} from './servers/server/server.component';
+import {ServersService} from './servers/servers.service';
 import {RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'users/:id/:name', component: UserComponent },
-  { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id', component: ServerComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent },
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UsersComponent, children: [
+      {path: ':id/:name', component: UserComponent},
 
+    ]},
+  {
+    path: 'servers', component: ServersComponent, children: [
+      {path: ':id', component: ServerComponent},
+      {path: ':id/edit', component: EditServerComponent}
+    ]
+  }
 ];
 
 @NgModule({
@@ -42,4 +45,5 @@ const appRoutes: Routes = [
   providers: [ServersService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
