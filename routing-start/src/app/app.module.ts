c7176760407d51617e6fe28bed9e053cd37d8a44
Vaerilius <tjohannest@gmail.com>
2019-08-07 08:45:15 +0300

    Practicing and some Common Gotchas

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -16,9 +16,10 @@ import {RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UserComponent },
+  { path: 'users', component: UsersComponent },
   { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent },
+  { path: 'servers/:id', component: ServerComponent },
   { path: 'servers/:id/edit', component: EditServerComponent },
 
 ];
--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ServersService } from '../servers.service';
 import {ActivatedRoute} from '@angular/router';
 
