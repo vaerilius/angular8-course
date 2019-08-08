2501b0fe133f4b6e6123adf45a9123979f817ea7
Vaerilius <tjohannest@gmail.com>
2019-08-08 08:26:45 +0300

    Protecting Child (Nested) Routes with canActivateChild

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -17,7 +17,10 @@ const appRoutes: Routes = [
 
     ]},
   {
-    path: 'servers', canActivate: [AuthGuardService], component: ServersComponent, children: [
+    path: 'servers',
+    // canActivate: [AuthGuardService],
+    canActivateChild: [AuthGuardService],
+    component: ServersComponent, children: [
       {path: ':id', component: ServerComponent},
       {path: ':id/edit', component: EditServerComponent}
     ]
--- routing-start/src/app/auth-guard.service..ts
+++ routing-start/src/app/auth-guard.service..ts
@@ -1,25 +1,29 @@
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
+import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot} from "@angular/router";
 import {Observable} from "rxjs/observable";
 import {Injectable} from "@angular/core";
 import {AuthService} from "./auth.service";
 
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate, CanActivateChild {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> |
     Promise<boolean> | boolean {
-    return  this.authService.isAuthenticated()
-    .then(
-      (authenticated: boolean) => {
-        if (authenticated) {
-          return true;
-        } else {
-          this.router.navigate(['/']);
-          return false;
+    return this.authService.isAuthenticated()
+      .then(
+        (authenticated: boolean) => {
+          if (authenticated) {
+            return true;
+          } else {
+            this.router.navigate(['/']);
+            return false;
+          }
         }
-      }
-    );
+      );
+  }
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean  {
+    return this.canActivate(childRoute, state);
   }
 }
