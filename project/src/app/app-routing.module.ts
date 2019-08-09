5b3f965256973da0e16a663439c8978c48be2733
Vaerilius <tjohannest@gmail.com>
2019-08-09 10:02:51 +0300

    edit component

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -6,12 +6,15 @@ import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
 import {RecipeItemComponent} from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
+import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {path: 'recipes', component: RecipesComponent, children: [
       {path: '', component: RecipeStartComponent},
+      {path: 'new', component: RecipeEditComponent},
       {path: ':id', component: RecipeDetailComponent},
+      {path: ':id/edit', component: RecipeEditComponent},
     ]},
   {path: 'shopping-list', component: ShoppingListComponent},
   {path: '**', component: RecipesComponent},
