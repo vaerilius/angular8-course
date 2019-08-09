da99d7ff2cc37086f7a2432eeb8dc6102979d3e7
Vaerilius <tjohannest@gmail.com>
2019-08-09 09:53:38 +0300

    get selected recipe from recipe-list

--- project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../../recipe.model';
-import {RecipeService} from '../../recipe.service';
+import {RecipeService} from "../../recipe.service";
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,8 +10,11 @@ import {RecipeService} from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Input() index: number;
+
 
   ngOnInit() {
+
   }
 
 }
