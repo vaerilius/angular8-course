ac0681bb66c7d7ecc179ef1d72a121d2626984aa
Vaerilius <tjohannest@gmail.com>
2019-08-12 08:33:03 +0300

    Errors & Completion

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -20,11 +20,23 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+        }
         count++;
       }, 1000);
     });
     this.firstObsSubscription = customIntervalObservable.subscribe(data => {
       console.log(data);
+    }, error => {
+
+      console.log(error);
+      alert(error);
+    }, () => {
+      console.log('Completed');
     });
   }
 
