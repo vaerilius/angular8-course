ca6b5748dbf9b8eccc7088af57d05d377673d446
Vaerilius <tjohannest@gmail.com>
2019-08-13 08:38:49 +0300

    Handling Radio Buttons

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   @ViewChild('f', {static: false}) signIn: NgForm;
   defaultQuestion = 'teacher';
   answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
