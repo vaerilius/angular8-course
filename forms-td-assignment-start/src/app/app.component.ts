41a490840ed7709638069561bb4da2f0bf1dc66d
Vaerilius <tjohannest@gmail.com>
2019-08-13 10:18:01 +0300

    Assignment

---
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f', {static: false}) signIn: NgForm;
+  submitted = false;
+  basic = 'basic';
+
+  onSubmit() {
+  this.submitted = true;
+    console.log(this.signIn);
+
+    this.signIn.reset();
+  }
+}
