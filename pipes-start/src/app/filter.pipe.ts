229add7d07f984899f7e5059367f025ff641f87a
Vaerilius <tjohannest@gmail.com>
2019-08-16 18:05:46 +0300

    Creating a Filter Pipe

---
+++ pipes-start/src/app/filter.pipe.ts
@@ -0,0 +1,23 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+
+  transform(value: any, filterString: string, propNAme: string): any {
+    if (value.length === 0 || filterString === '') {
+      return value;
+    }
+    const resultArray = [];
+    for (const item of value) {
+      if (item[propNAme] === filterString) {
+        resultArray.push(item);
+      }
+    }
+    return resultArray;
+
+  }
+
+}
