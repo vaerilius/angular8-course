75e3bc0c2a48d926868464c8bbe1ba9c0547f002
Vaerilius <tjohannest@gmail.com>
2019-08-16 08:48:42 +0300

    image bug

--- project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -36,6 +36,5 @@ export class RecipeDetailComponent implements OnInit {
   onDeleteRecipe() {
     this.recipeService.deleteRecipe(this.id);
     this.router.navigate(['../'], {relativeTo: this.route});
-
   }
 }
