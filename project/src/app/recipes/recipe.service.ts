2556b8ddf9fb1af6d05cda2bb88eace8c9c45ac0
Vaerilius <tjohannest@gmail.com>
2019-08-20 09:39:44 +0300

    Transforming Response Data

--- project/src/app/recipes/recipe.service.ts
+++ project/src/app/recipes/recipe.service.ts
@@ -8,23 +8,24 @@ import {Subject} from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('Tasty Burger',
-      'Nam Nam',
-      'https://images.unsplash.com/photo-1548946522-4a313e8972a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1340&q=80',
-      [
-        new Ingredient('Meat', 2),
-        new Ingredient('Bread', 1),
-        new Ingredient('chili', 1)
-      ]),
-    new Recipe('Chili Burger',
-      'HOT HOT!!',
-      'https://images.unsplash.com/photo-1551782450-a2132b4ba21d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-      [
-        new Ingredient('Meat', 3),
-        new Ingredient('French fries', 1),
-        new Ingredient('banana', 2)]
-    )];
+  // private recipes: Recipe[] = [
+  //   new Recipe('Tasty Burger',
+  //     'Nam Nam',
+  //     'https://images.unsplash.com/photo-1548946522-4a313e8972a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1340&q=80',
+  //     [
+  //       new Ingredient('Meat', 2),
+  //       new Ingredient('Bread', 1),
+  //       new Ingredient('chili', 1)
+  //     ]),
+  //   new Recipe('Chili Burger',
+  //     'HOT HOT!!',
+  //     'https://images.unsplash.com/photo-1551782450-a2132b4ba21d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+  //     [
+  //       new Ingredient('Meat', 3),
+  //       new Ingredient('French fries', 1),
+  //       new Ingredient('banana', 2)]
+  //   )];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {
   }
--- project/src/app/shared/data-storage.service.ts
+++ project/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +22,11 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http.get<Recipe[]>('https://recipe-book-e2cc3.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+        });
+      }))
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
         console.log(recipes);
