d220e6ecef2ff2233ade735c52c438eb417c6f82
Vaerilius <tjohannest@gmail.com>
2019-08-20 09:21:55 +0300

    Storing Recipes

--- project/src/app/header/header.component.ts
+++ project/src/app/header/header.component.ts
@@ -1,9 +1,15 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {DataStorageService} from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  constructor(private dataStorage: DataStorageService) {
+  }
 
+  onSaveData() {
+    this.dataStorage.storeRecipes();
+  }
 }
--- project/src/app/shared/data-storage.service.ts
+++ project/src/app/shared/data-storage.service.ts
@@ -1,11 +1,21 @@
 import {Injectable} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
+import {RecipeService} from '../recipes/recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
-  constructor(private http: HttpClientModule) {
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  }
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://recipe-book-e2cc3.firebaseio.com/recipes.json', recipes)
+      .subscribe(response => {
+        console.log(response);
+      });
   }
 
 
