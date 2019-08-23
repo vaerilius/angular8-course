0c478f35ee444ba6d9ceb7b19fe42c2d17c73659
Vaerilius <tjohannest@gmail.com>
2019-08-23 12:53:13 +0300

    Adding Auto-Login

--- project/src/app/auth/auth.service.ts
+++ project/src/app/auth/auth.service.ts
@@ -43,6 +43,27 @@ export class AuthService {
     this.router.navigate(['/auth']);
   }
 
+  autoLogin() {
+    const userData: {
+      email: string,
+      id: string,
+      _token: string,
+      _tokenExpirationDate: string,
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   login(email: string, password: string) {
     return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCy85lcn8JFrV4u1I36sreqfyeeyiVJI2Y',
       {
@@ -68,6 +89,7 @@ export class AuthService {
       toke,
       expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errResponse: HttpErrorResponse) {
