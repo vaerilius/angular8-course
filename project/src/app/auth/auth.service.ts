426d46937cd5b8ce6b6fa9602c6596e5f154331a
Vaerilius <tjohannest@gmail.com>
2019-08-23 11:25:22 +0300

    Adding the Token to Outgoing Requests

--- project/src/app/auth/auth.service.ts
+++ project/src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {catchError, tap} from 'rxjs/operators';
-import {Subject, throwError} from 'rxjs';
+import {BehaviorSubject, Subject, throwError} from 'rxjs';
 import {User} from './user.model';
 
 export interface AuthResponseData {
@@ -17,7 +17,8 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-  user = new Subject<User>();
+  user = new BehaviorSubject<User>(null);
+
 
   constructor(private http: HttpClient) {
   }
--- project/src/app/shared/data-storage.service.ts
+++ project/src/app/shared/data-storage.service.ts
@@ -1,27 +1,35 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
-import {map, tap} from 'rxjs/operators';
+import {exhaustMap, map, take, tap} from 'rxjs/operators';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  constructor(private http: HttpClient, private recipeService: RecipeService,
+              private authService: AuthService) {
   }
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
-    this.http.put('https://recipe-book-e2cc3.firebaseio.com/recipes.json', recipes)
+    this.http.put('https://recipe-book-e2cc3.firebaseio.com/recipes.json',
+      recipes)
       .subscribe(response => {
         console.log(response);
       });
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>('https://recipe-book-e2cc3.firebaseio.com/recipes.json')
+    return this.authService.user.pipe(take(1), exhaustMap(user => {
+      return this.http.get<Recipe[]>('https://recipe-book-e2cc3.firebaseio.com/recipes.json',
+        {
+          params: new HttpParams().set('auth', user.token)
+        })
+    }))
       .pipe(map(recipes => {
         return recipes.map(recipe => {
           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
