a03a98eec06b4800994bf1a2bf9fbbd3c18cfc02
Vaerilius <tjohannest@gmail.com>
2019-08-29 10:34:45 +0300

    More Lazy Loading

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -3,7 +3,14 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: '/recipes', pathMatch: 'full'}
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  // {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'}
+  {path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)},
+  {
+    path: 'shopping-list',
+    loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)
+  },
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
 ];
 
 @NgModule({
