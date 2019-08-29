32882f784f73eb46b0651af72de4f0ac82ea339f
Vaerilius <tjohannest@gmail.com>
2019-08-29 09:48:11 +0300

    Understanding the Core Module

--- project/src/app/recipes/recipe.service.ts
+++ project/src/app/recipes/recipe.service.ts
@@ -4,7 +4,7 @@ import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
 import {Subject} from 'rxjs';
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
