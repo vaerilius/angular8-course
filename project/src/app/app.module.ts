e34e2d20eabef0adef26f4a22baf8402342355d8
Vaerilius <tjohannest@gmail.com>
2019-09-03 08:37:30 +0300

    Setting Up the NgRx Store

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {StoreModule} from '@ngrx/store';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from './app-routing.module';
 import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core.module';
+import {shoppingListReducer} from './shopping-list/store/shopping-list.reducer';
 
 
 @NgModule({
@@ -19,9 +21,9 @@ import {CoreModule} from './core.module';
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer})
   ],
-  // providers: [LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
